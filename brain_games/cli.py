c7774f5d3e55221b057d81078615cce1f3bf6b5c
Elena Mikitenko <lenyska07@mail.ru>
2021-07-16 21:11:05 +0300

    modify brain_games

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -4,11 +4,6 @@ import prompt
 
 def welcome_user():
     print("Welcome to the Brain Games, my friend!")
-    name = prompt.string('May I have your name? ')
+    name = prompt.string('May I have your name?')
     print('Hello, {}'.format(name))
     return name
-
-def main(): welcome_user()
-
-if __name__ != 'main':
-    main()
