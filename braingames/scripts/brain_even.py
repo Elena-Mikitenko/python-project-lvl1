3c3110183fc50c9cd8759bdbb0e9a56942a7ec64
Elena Mikitenko <lenyska07@mail.ru>
2021-07-24 22:16:41 +0300

    modify brain-even

--- braingames/scripts/brain_even.py
+++ braingames/scripts/brain_even.py
@@ -3,25 +3,34 @@ from random import randint
 
 
 def main():
+    count = 1
     print('Welcome to the Brain Games! ')
     name = prompt.string('May I have your name? ')
     print('Hello, {0}!'.format(name))
+    print("Answer 'yes' if the number is even, otherwise answer 'no'.")
+
+    number = randint(0, 60)
+    result = ''
+
+    while count < 3:
+        count += 1
 
 
-    number = randint(0, 100)
+        if number % 2 == 0:
+            correct_answer = 'yes'
+        else:
+            correct_answer = 'no'
+        print('Question: ' + str(number))
+        answer = input('Your answer: ')
 
 
-    if number % 2 == 0:
-        correct_answer = 'yes'
-    else:
-        correct_answer = 'no'
-    print('Question: ' + str(number))
-    answer = input('Your answer: ')
-
-
-    if answer == 'yes' and number % 2 == 0 or answer == 'no' and number % 2 == 1:
-        print('Correct!')
-        return True
-    else:
-        print("'" + answer + " ' is wrong answer ;(. Correct answer was " + correct_answer + ".\nLet's try again " + name + "!")
-        return False
+        if answer == 'yes' and number % 2 == 0 and count < 3 or answer == 'no' and number % 2 == 1 and count < 3:
+            print('Correct!')
+            return True
+        elif answer == 'yes' and number % 2 == 1 and count < 3 or answer == 'no' and number % 2 == 0 and count < 3:
+            print("'" + answer + " ' is wrong answer ;(. Correct answer was " + correct_answer + ".\nLet's try again, " + name + "!")
+            return False
+        elif result == True and count >= 3:
+            print('Congatulations, ' + name + '!')
+        elif result == False:
+            print("Let's try again, " + name + "!")
