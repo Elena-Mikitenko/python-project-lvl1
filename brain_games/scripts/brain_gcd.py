417403538af171a71675f2ee1ba7fcad85e65681
Elena Mikitenko <lenyska07@mail.ru>
2021-07-31 17:14:17 +0300

    add brain-gcd

---
+++ brain_games/scripts/brain_gcd.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+import random
+from brain_games.cli import welcome_user
+from math import gcd
+
+
+def game_gcd():
+    name = welcome_user()
+    print('Find the greatest common divisor of given numbers.')
+    count = 1
+    answer = ''
+    divisor = ''
+
+    while count <= 3:
+        count += 1
+        number1, number2 = random.randint(1, 100), random.randint(1, 100)
+        given_numbers = str(number1) + ' ' + str(number2)
+        print('Question: ' + str(given_numbers))
+        answer = input('Your answer: ')
+        divisor = gcd(number1, number2)
+
+        if answer == str(divisor):
+            print('Correct!')
+        else:
+            print(("'" + str(answer) + "' is wrong answer ;(. "
+                   "Correct answer was'" + str(divisor) + "'. \nLet's "
+                   "try again, " + name + "!"))
+    if count > 3 and answer == str(divisor):
+        print('Congratulations, ' + name + '!')
