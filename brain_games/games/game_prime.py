3455608e1c2d1f07f3fc32f50abba00e1ec6c347
Elena Mikitenko <lenyska07@mail.ru>
2021-08-16 22:54:42 +0300

    modify game-prime once again

--- brain_games/games/game_prime.py
+++ brain_games/games/game_prime.py
@@ -1,22 +1,19 @@
 #!/usr/bin/env python
 
 from random import randint
-import math
 
 games_purpose = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
 def game_prime(numbers_question):
-    number = numbers_question
     first_prime_number = 2
     if numbers_question < first_prime_number:
         return 'no'
-
-    while number >= first_prime_number:
-        number -= 1
-        divisor = math.gcd(numbers_question, number)
-        if 1 < divisor < numbers_question:
+    divisor = 2
+    while divisor < numbers_question:
+        if numbers_question % divisor == 0:
             return 'no'
+        divisor += 1
     return 'yes'
 
 
